5e9fb46c000b20caa5914be7d842370a6a3eb64f
Gianmaria Fadda <faddagianmaria10@gmail.com>
2024-10-23 14:44:31 +0200

    spostato ComicController in cartella Admin

--- app/Http/Controllers/Admin/ComicController.php
+++ app/Http/Controllers/Admin/ComicController.php
@@ -1,9 +1,10 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Admin;
 
-use App\Models\Comic;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\Comic;
 
 class ComicController extends Controller
 {
