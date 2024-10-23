d337174826fca63c270fcfec3df8679072350e2a
Gianmaria Fadda <faddagianmaria10@gmail.com>
2024-10-23 15:21:33 +0200

    corretto errore di scrittura

--- database/seeders/ComicSeeder.php
+++ database/seeders/ComicSeeder.php
@@ -5,13 +5,34 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+use App\Models\Comic;
+
 class ComicSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run()
     {
-        //
+        Comic::truncate();
+
+        $comics = config('comics');
+
+        foreach ($comics as $singleComic) {
+            $comic = new Comic ();
+            $comic->title = $singleComic['title'];
+            $comic->description = $singleComic[ 'description'];
+            $comic->thumb = $singleComic ['thumb'];
+            $priceNumber = floatval(substr($singleComic['price'], 1));
+            $comic->price = $priceNumber;
+            $comic->series = $singleComic['series'];
+            $comic->sale_date = $singleComic['sale_date'];
+            $comic->type = $singleComic ['type'];
+            $jsonArtists = json_encode($singleComic['artists']);
+            $comic->artists = $jsonArtists;
+            $implodedWriters = implode('|', $singleComic['writers']);
+            $comic->writers = $implodedWriters;
+            $comic->save();
+        }
     }
 }
