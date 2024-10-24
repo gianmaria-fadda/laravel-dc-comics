8f54ec81fb676aca4ef81f2be1270c29935cabeb
Gianmaria Fadda <faddagianmaria10@gmail.com>
2024-10-24 16:59:30 +0200

    Completato validazione su create e cominciato su edit

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -7,37 +7,72 @@
     Crea Comics
 </h1>
 
+@if ($errors->any())
+    <div class="alert alert-danger my-4">
+      <ul class="mb-0">
+        @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+        @endforeach
+      </ul>
+    </div>
+@endif
+
 <form action="{{ route('comics.store') }}" method="POST">
     @csrf
 
     <div class="mb-3">
         <label for="title" class="form-label">Titolo <span class="text-danger">*</span></label>
-        <input type="text" class="form-control" id="title" name="title" required maxlength="128" placeholder="Inserisci qui il titolo...">
+        <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" name="title" required maxlength="128" placeholder="Inserisci qui il titolo...">
+        @error('title')
+          <div class="alert alert-danger mt-1">
+            Errore Titolo: {{ $message }}
+          </div>
+        @enderror
       </div>
 
       <div class="mb-3">
         <label for="thumb" class="form-label">Immagine</label>
-        <input type="text" class="form-control" id="thumb" name="thumb" maxlength="2048" placeholder="Inserisci qui la copertina...">
+        <input type="text" class="form-control @error('thumb') is-invalid @enderror" id="thumb" name="thumb" maxlength="2048" placeholder="Inserisci qui la copertina...">
+        @error('thumb')
+          <div class="alert alert-danger mt-1">
+            Errore Img: {{ $message }}
+          </div>
+        @enderror
       </div>
 
       <div class="mb-3">
         <label for="price" class="form-label">Prezzo <span class="text-danger">*</span></label>
-        <input type="number" class="form-control" id="price" name="price" required max="999.99" step="0.01" placeholder="Inserisci qui il prezzo...">
+        <input type="number" class="form-control @error('price') is-invalid @enderror" id="price" name="price" required max="999.99" step="0.01" placeholder="Inserisci qui il prezzo...">
+        @error('price')
+          <div class="alert alert-danger mt-1">
+            Errore Prezzo: {{ $message }}
+          </div>
+        @enderror
       </div>
 
       <div class="mb-3">
         <label for="series" class="form-label">Serie <span class="text-danger">*</span></label>
-        <input type="text" class="form-control" id="series" name="series" required maxlength="64" placeholder="Inserisci qui la serie...">
+        <input type="text" class="form-control @error('series') is-invalid @enderror" id="series" name="series" required maxlength="64" placeholder="Inserisci qui la serie...">
+        @error('series')
+          <div class="alert alert-danger mt-1">
+            Errore Serie: {{ $message }}
+          </div>
+        @enderror
       </div>
 
       <div class="mb-3">
         <label for="sale_date" class="form-label">Data Vendita</label>
-        <input type="text" class="form-control" id="sale_date" name="sale_date" placeholder="Inserisci qui la data di vendita...">
+        <input type="text" class="form-control @error('sale_date') is-invalid @enderror" id="sale_date" name="sale_date" placeholder="Inserisci qui la data di vendita...">
+        @error('sale_date')
+          <div class="alert alert-danger mt-1">
+            Errore Data della Vendita: {{ $message }}
+          </div>
+        @enderror
       </div>
 
       <div class="mb-3">
         <label for="type" class="form-label">Tipo <span class="text-danger">*</span></label>
-        <select class="form-select" id="type" name="type">
+        <select class="form-select @error('type') is-invalid @enderror" id="type" name="type">
             <option select disabled>Seleziona un tipo...</option>
             <option value="comic book">Fumetto</option>
             <option value="graphic novel">Graphic Novel</option>
@@ -46,7 +81,12 @@
 
       <div class="mb-3">
         <label for="artists" class="form-label">Artisti</label>
-        <textarea class="form-control" id="artists" name="artists" aria-describedby="artist-helper" placeholder="Inserisci gli Artisti..."></textarea>
+        <textarea class="form-control @error('artists') is-invalid @enderror" id="artists" name="artists" aria-describedby="artist-helper" placeholder="Inserisci gli Artisti..."></textarea>
+        @error('artists')
+          <div class="alert alert-danger mt-1">
+            Errore Artisti: {{ $message }}
+          </div>
+        @enderror
         <div id="artist-helper" class="form-text">
             Inserisci i Nomi degli Artisti separati da una virgola
           </div>
@@ -54,7 +94,12 @@
 
       <div class="mb-3">
         <label for="writers" class="form-label">Scrittori</label>
-        <textarea class="form-control" id="writers" name="writers" aria-describedby="writers-helper" placeholder="Inserisci gli Scrittori..."></textarea>
+        <textarea class="form-control @error('writers') is-invalid @enderror" id="writers" name="writers" aria-describedby="writers-helper" placeholder="Inserisci gli Scrittori..."></textarea>
+        @error('writers')
+          <div class="alert alert-danger mt-1">
+            Errore Scrittori: {{ $message }}
+          </div>
+        @enderror
         <div id="writers-helper" class="form-text">
             Inserisci i Nomi degli Scrittori separati da una virgola
           </div>
@@ -62,7 +107,12 @@
 
       <div class="mb-3">
         <label for="description" class="form-label">Descrizione <span class="text-danger">*</span></label>
-        <textarea class="form-control" id="description" name="description" rows="3" required maxlength="4096" placeholder="Inserisci la Descrizione..."></textarea>
+        <textarea class="form-control @error('description') is-invalid @enderror" id="description" name="description" rows="3" required maxlength="4096" placeholder="Inserisci la Descrizione..."></textarea>
+        @error('description')
+          <div class="alert alert-danger mt-1">
+            Errore Descrizione: {{ $message }}
+          </div>
+        @enderror
       </div>
 
       <div>
--- resources/views/comics/edit.blade.php
+++ resources/views/comics/edit.blade.php
@@ -7,34 +7,69 @@
   Modifica {{ $comic->title }}
 </h1>
 
+@if ($errors->any())
+    <div class="alert alert-danger my-4">
+      <ul class="mb-0">
+        @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+        @endforeach
+      </ul>
+    </div>
+@endif
+
 <form action="{{ route('comics.update', ['comic' => $comic->id]) }}" method="POST">
     @csrf
     @method('PUT')
 
     <div class="mb-3">
         <label for="title" class="form-label">Titolo <span class="text-danger">*</span></label>
-        <input type="text" class="form-control" id="title" name="title" required maxlength="128" value="{{ $comic->title }}" placeholder="Inserisci qui il titolo...">
-      </div>
+        <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" name="title" required maxlength="128" value="{{ $comic->title }}" placeholder="Inserisci qui il titolo...">
+        @error('title')
+        <div class="alert alert-danger mt-1">
+          Errore Titolo: {{ $message }}
+        </div>
+      @enderror
+    </div>
 
       <div class="mb-3">
         <label for="thumb" class="form-label">Immagine</label>
-        <input type="text" class="form-control" id="thumb" name="thumb" maxlength="2048" value="{{ $comic->thumb }}" placeholder="Inserisci qui la copertina...">
-      </div>
+        <input type="text" class="form-control @error('thumb') is-invalid @enderror" id="thumb" name="thumb" maxlength="2048" value="{{ $comic->thumb }}" placeholder="Inserisci qui la copertina...">
+        @error('thumb')
+        <div class="alert alert-danger mt-1">
+          Errore Img: {{ $message }}
+        </div>
+      @enderror
+    </div>
 
       <div class="mb-3">
         <label for="price" class="form-label">Prezzo <span class="text-danger">*</span></label>
-        <input type="number" class="form-control" id="price" name="price" required max="999.99" step="0.01" value="{{ $comic->price }}" placeholder="Inserisci qui il prezzo...">
-      </div>
+        <input type="number" class="form-control @error('price') is-invalid @enderror" id="price" name="price" required max="999.99" step="0.01" value="{{ $comic->price }}" placeholder="Inserisci qui il prezzo...">
+        @error('price')
+        <div class="alert alert-danger mt-1">
+          Errore Prezzo: {{ $message }}
+        </div>
+      @enderror
+    </div>
 
       <div class="mb-3">
         <label for="series" class="form-label">Serie <span class="text-danger">*</span></label>
-        <input type="text" class="form-control" id="series" name="series" required maxlength="64" value="{{ $comic->series }}" placeholder="Inserisci qui la serie...">
-      </div>
+        <input type="text" class="form-control @error('series') is-invalid @enderror" id="series" name="series" required maxlength="64" value="{{ $comic->series }}" placeholder="Inserisci qui la serie...">
+        @error('series')
+        <div class="alert alert-danger mt-1">
+          Errore Serie: {{ $message }}
+        </div>
+      @enderror
+    </div>
 
       <div class="mb-3">
         <label for="sale_date" class="form-label">Data Vendita</label>
-        <input type="text" class="form-control" id="sale_date" name="sale_date" value="{{ $comic->sale_date }}" placeholder="Inserisci qui la data di vendita...">
-      </div>
+        <input type="text" class="form-control @error('sale_date') is-invalid @enderror" id="sale_date" name="sale_date" value="{{ $comic->sale_date }}" placeholder="Inserisci qui la data di vendita...">
+        @error('sale_date')
+        <div class="alert alert-danger mt-1">
+          Errore Data della Vendita: {{ $message }}
+        </div>
+      @enderror
+    </div>
 
       <div class="mb-3">
         <label for="type" class="form-label">Tipo <span class="text-danger">*</span></label>
@@ -54,15 +89,20 @@
 
       <div class="mb-3">
         <label for="artists" class="form-label">Artisti</label>
-        <textarea class="form-control" id="artists" name="artists" aria-describedby="artist-helper"  value="{{ $comic->artists }}" placeholder="Inserisci gli Artisti..."></textarea>
+        <textarea class="form-control @error('artists') is-invalid @enderror" id="artists" name="artists" aria-describedby="artist-helper"  value="{{ $comic->artists }}" placeholder="Inserisci gli Artisti..."></textarea>
         <div id="artist-helper" class="form-text">
             Inserisci i Nomi degli Artisti separati da una virgola
           </div>
+          @error('artists')
+          <div class="alert alert-danger mt-1">
+            Errore Data della Vendita: {{ $message }}
+          </div>
+        @enderror
       </div>
 
       <div class="mb-3">
         <label for="writers" class="form-label">Scrittori</label>
-        <textarea class="form-control" id="writers" name="writers" aria-describedby="writers-helper" value="{{ $comic->writers }}" placeholder="Inserisci gli Scrittori..."></textarea>
+        <textarea class="form-control @error('writers') is-invalid @enderror" id="writers" name="writers" aria-describedby="writers-helper" value="{{ $comic->writers }}" placeholder="Inserisci gli Scrittori..."></textarea>
         <div id="writers-helper" class="form-text">
             Inserisci i Nomi degli Scrittori separati da una virgola
           </div>
@@ -70,7 +110,7 @@
 
       <div class="mb-3">
         <label for="description" class="form-label">Descrizione <span class="text-danger">*</span></label>
-        <textarea class="form-control" id="description" name="description" rows="3" required maxlength="4096" value="{{ $comic->description }}" placeholder="Inserisci la Descrizione..."></textarea>
+        <textarea class="form-control @error('series') is-invalid @enderror"description id="description" name="description" rows="3" required maxlength="4096" value="{{ $comic->description }}" placeholder="Inserisci la Descrizione..."></textarea>
       </div>
 
       <div>
